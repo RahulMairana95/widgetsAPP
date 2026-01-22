4009886f298b735f04307b9c17e3a7d1c21031d7
RahilMairana95 <rrahulmairana@gmail.com>
2026-01-22 01:00:11 -0400

    configuracion de GoRouter

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
+//GoRouter configuracion
+
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomeScreen(),
+    ),
+
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => ButtonsScreen(),
+    ),
+
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => CardsScreen(),
+    ),
+  ],
+);
