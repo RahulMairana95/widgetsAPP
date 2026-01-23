7bc30160d959f0955713247706d607767824ba9d
RahilMairana95 <rrahulmairana@gmail.com>
2026-01-23 02:35:31 -0400

    Screen creadas y barril actualizado

---
+++ lib/presentation/screens/snackbar_screen/snackbar_screen.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class SnackBar extends StatelessWidget {
+  static const name = 'snackbar_screen';
+  const SnackBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
